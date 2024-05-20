e25dc63e833fc290d00ccbebb82c717b0fb7ce1b
Karl <westerholm.karl@gmail.com>
2024-05-20 11:47:29 +0200

    Moved chatbot to root directory with changes

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,6 +1,9 @@
+import json
 import os
+import re
 from typing import Literal
 
+import markdown
 import requests
 from dotenv import load_dotenv
 
@@ -51,7 +54,7 @@ class ChatBot:
         new_message = {"role": role, "content": message}
         self._payload["messages"].append(new_message)
 
-    def send_message(self, message: str):
+    def send_message(self, message: str, get_full_response: bool = False):
         self._add_message_to_payload(message=message, role="user")
 
         headers = {
@@ -66,11 +69,35 @@ class ChatBot:
             raise SystemExit(f"Failed to make the request. Error: {e}")
 
         response = response.json()
+        if get_full_response:
+            return response
+
         answer = response["choices"][0]["messages"][1]["content"]
 
         self._add_message_to_payload(message=answer, role="assistant")
 
-        return answer
+        bot_message = response["choices"][0]["messages"][1]["content"]
+        citations = json.loads(response["choices"][0]["messages"][0]["content"])["citations"]
+
+        message_with_citations = self.add_citations(bot_message, citations)
+        formatted_bot_message = markdown.markdown(message_with_citations)
+
+        return formatted_bot_message
+
+    def add_citations(self, my_string, urls):
+        # Define a function to replace [doc*] with corresponding url
+        def replace_with_url(match):
+            index = int(match.group(1)) - 1  # Subtract 1 because list index starts from 0
+            if index < len(urls):
+                url = urls[index]["url"]
+                return f"{match.group(0)}({url})"
+            else:
+                return match.group(0)  # If index is out of range, return the original string
+
+        # Use re.sub with a function as replacer
+        my_formatted_string = re.sub(r"\[doc(\d+)\]", replace_with_url, my_string)
+
+        return my_formatted_string
 
 
 def main():
@@ -81,7 +108,7 @@ def main():
     chatbot = ChatBot(api_key=GPT4V_KEY, endpoint=GPT4V_ENDPOINT)
     while True:
         user_query = input("Please enter a query: ")
-        answer = chatbot.send_message(user_query)
+        answer = chatbot.send_message(user_query, get_full_response=False)
 
         print("\n\nMessage:\n", answer, "\n")
 
